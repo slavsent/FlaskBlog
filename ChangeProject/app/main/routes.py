2050730c3235c90f53f7fed4c176671fc17bd8ae
Slav <ricar@mail.ru>
2024-01-19 15:15:43 +0300

    add elasticsearch

--- ChangeProject/app/main/routes.py
+++ ChangeProject/app/main/routes.py
@@ -1,6 +1,6 @@
 from FlaskBlog.ChangeProject.app import db
-from flask import render_template, flash, redirect, url_for, request, current_app
-from FlaskBlog.ChangeProject.app.main.forms import EditProfileForm, PostForm, EmptyForm
+from flask import render_template, flash, redirect, url_for, request, current_app, g
+from FlaskBlog.ChangeProject.app.main.forms import EditProfileForm, PostForm, EmptyForm, SearchForm
 from flask_login import current_user, login_user, logout_user, login_required
 from urllib.parse import urlparse
 from FlaskBlog.ChangeProject.app.models import User, Post
@@ -73,6 +73,7 @@ def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.now(timezone.utc)
         db.session.commit()
+        g.search_form = SearchForm()
 
 
 @bp.route('/edit_profile', methods=['GET', 'POST'])
@@ -142,4 +143,20 @@ def explore():
     prev_url = url_for('main.explore', page=posts.prev_num) \
         if posts.has_prev else None
     return render_template("index.html", title='Explore', posts=posts.items,
+                           next_url=next_url, prev_url=prev_url)
+
+
+@bp.route('/search')
+@login_required
+def search():
+    if not g.search_form.validate():
+        return redirect(url_for('main.explore'))
+    page = request.args.get('page', 1, type=int)
+    posts, total = Post.search(g.search_form.q.data, page,
+                               current_app.config['POSTS_PER_PAGE'])
+    next_url = url_for('main.search', q=g.search_form.q.data, page=page + 1) \
+        if total > page * current_app.config['POSTS_PER_PAGE'] else None
+    prev_url = url_for('main.search', q=g.search_form.q.data, page=page - 1) \
+        if page > 1 else None
+    return render_template('search.html', title=_('Search'), posts=posts,
                            next_url=next_url, prev_url=prev_url)
